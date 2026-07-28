49729514a2a32b75575a9a08ceb63d8a0466e523
Anshul Dave <ahdave1573@gmail.com>
2026-07-28 11:46:26 +0530

    Add config and middleware folders

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "childrens_store";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed.");
+}
+
+?>
---
+++ middleware/require_admin.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../includes/session.php';
+
+if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin'){
+    header("Location: ../login.php");
+    exit;
+}
